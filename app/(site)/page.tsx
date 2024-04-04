d511d58b264561eb9e6a8f9ea7fe6f81c05b4337
Pavankumar Uppar <pavankumar.ai20@bmsce.ac.in>
2024-04-04 20:54:36 +0530

    create auth component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
     return (
@@ -27,6 +28,7 @@ export default function Home() {
                 Sign in to your account
             </h2>
         </div>
+        <AuthForm />
       </div>
     );
   }
